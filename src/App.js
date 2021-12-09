6edfcab0df1796f58d61ccde5c78334f7210b65f
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2021-12-08 23:28:48 -0800

    working prototype

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            squares: Array(2).fill(null),
+            squares: Array(3).fill(null),
         }
     }
     // @TODO Pass this function to each individual square to call once they are clicked.
     handleClicked(i) {
         const squares = this.state.squares.slice();
+        if (squares[i] === true) {
+            alert("You lose, button clicked already");
+        }
         squares[i] = true;
         this.setState({
             squares: squares,
@@ -19,16 +22,20 @@ class App extends React.Component {
 
     renderSquare(i) {
         return (
-            <Square value={this.state.squares[i]}/>
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClicked(i)}
+            />
         );
     }
 
     render() {
         return (
             <div className="app">
+                {this.renderSquare(0)}
                 {this.renderSquare(1)}
-                {this.renderSquare(1)}
-                {console.table(this.state.squares)}
+                {this.renderSquare(2)}
+                <button onClick={() => console.table(this.state.squares)}>Print Status</button>
             </div>
         );
     }
@@ -36,8 +43,7 @@ class App extends React.Component {
 
 function Square(props) {
     return (
-        <button className="square">
-            {props.value}
+        <button className="square" onClick={props.onClick}>
         </button>
     );
 }
