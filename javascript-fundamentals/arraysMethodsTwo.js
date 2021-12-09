65222f1646a78178cd0c116b6f094d36fc123bbf
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2021-12-08 16:39:07 -0800

    structure

---
+++ javascript-fundamentals/arraysMethodsTwo.js
@@ -0,0 +1,5 @@
+const coolArray = Array(9).fill(0);
+console.table(coolArray);
+
+const edited = coolArray.slice();
+console.table(edited);
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './styling/App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Hello World</h1>
+        </div>
+    );
+}
+
+export default App;
