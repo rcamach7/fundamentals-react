119b7ce35f999916b27503bda5091b11c96ed6a4
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2021-12-08 18:21:27 -0800

    basic square set up for testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import './styling/App.css';
+import React from "react";
 
-function App() {
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(2).fill(null),
+        }
+    }
+    // @TODO Pass this function to each individual square to call once they are clicked.
+    handleClicked(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = true;
+        this.setState({
+            squares: squares,
+        })
+    }
+
+    renderSquare(i) {
+        return (
+            <Square value={this.state.squares[i]}/>
+        );
+    }
+
+    render() {
+        return (
+            <div className="app">
+                {this.renderSquare(1)}
+                {this.renderSquare(1)}
+                {console.table(this.state.squares)}
+            </div>
+        );
+    }
+}
+
+function Square(props) {
     return (
-        <div className="App">
-            <h1>Hello World, we have made changes!</h1>
-        </div>
+        <button className="square">
+            {props.value}
+        </button>
     );
 }
 
